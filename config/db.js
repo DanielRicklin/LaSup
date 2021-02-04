cb95d7ae24173a69e3a53b513f3138f0b3fed592
DanielRicklin <ricklin.daniel@outlook.fr>
2021-02-04 13:25:45 +0100

    g

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect("mongodb://lasup:azerazer@127.0.0.1/lasup", {
+      const conn = await mongoose.connect(process.env.MONGO_URI, {
         useUnifiedTopology: true,
         useNewUrlParser: true,
         useCreateIndex: true,
